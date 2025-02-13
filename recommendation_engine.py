d9e56279bf51e731f9be391621b20256fd5ab661
EugeneDvoryankov <kolontayev1@gmail.com>
2025-02-13 12:32:14 +0000

    Add dummy recommendation engine file

---
+++ recommendation_engine.py
@@ -0,0 +1,9 @@
+# recommendation_engine.py
+
+def get_recommendations(user_id):
+    """
+    Dummy recommendation engine.
+    For now, it simply returns a static list of book IDs.
+    """
+    # TODO: Retrieve user data and generate real recommendations
+    return ["OL123M", "OL456M", "OL789M"]
